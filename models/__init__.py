e5e9d6fa391b743b8509dd92d2c5cc1a988f731b
DeepRecursion <gtmehrer@gmail.com>
2019-10-17 15:56:17 -0700

    update imports

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
-from convbnrelu import ConvBNReLU
-from envnet import EnvNet as envnet
-from envnetv2 import EnvNetv2 as envnetv2
+from .convbnrelu import ConvBNReLU
+from .envnet import EnvNet as envnet
+from .envnetv2 import EnvNetv2 as envnetv2
